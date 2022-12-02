8cae43b7de7dda80e42c5dbd02426007cf1b23c4
Sascha Dietrich <sascha_dietrich@outlook.com>
2022-12-02 18:37:59 +0100

    config: eslint for cypress

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,4 +11,18 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
   },
+
+  plugins: ["cypress"],
+
+  rules: {
+    "cypress/no-assigning-return-values": "error",
+    "cypress/no-unnecessary-waiting": "error",
+    "cypress/assertion-before-screenshot": "warn",
+    "cypress/no-force": "warn",
+    "cypress/no-async-tests": "error",
+    "cypress/no-pause": "error",
+  },
+  env: {
+    "cypress/globals": true,
+  },
 };
