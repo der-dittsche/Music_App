8722ff34764a4a979f21f20a52dd9c331002f569
Sascha Dietrich <sascha_dietrich@outlook.com>
2022-12-03 22:48:18 +0100

    feat: add upload function as component, add config in firestore

--- src/stores/firebase.js
+++ src/stores/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAJpMj8M9bJmmPxFoR1Pk8ezOKr7BZW2fo',
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { db, auth };
+export { db, auth, storage };
