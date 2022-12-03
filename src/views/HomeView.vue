ca4a7a7e10545ce2c8d6b67f4ee4ba66e9df5493
Sascha Dietrich <sascha_dietrich@outlook.com>
2022-12-03 10:00:31 +0100

    feat: finish basic layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,13 @@
 <template>
   <h1>Hallo Opa</h1>
+  <div class="container">
+    <p>Listen to you music</p>
+    <ul>
+      <li>Songname 1 <button>play</button></li>
+      <li>Songname 2 <button>play</button></li>
+      <li>Songname 3 <button>play</button></li>
+      <li>Songname 4 <button>play</button></li>
+    </ul>
+  </div>
 </template>
 <script></script>
