4b7f4e89386d2264e5f98f56200cb757a61abf8a
Sascha Dietrich <sascha_dietrich@outlook.com>
2022-12-07 19:04:04 +0100

    feat: add setup for firebase local emulator

--- src/stores/firebase.js
+++ src/stores/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getStorage, connectStorageEmulator } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAJpMj8M9bJmmPxFoR1Pk8ezOKr7BZW2fo',
@@ -11,6 +11,24 @@ const firebaseConfig = {
   messagingSenderId: '306035918224',
   appId: '1:306035918224:web:8d28407ba64bb7e8e55f15',
 };
+
+if (import.meta.env.DEV) {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyAJpMj8M9bJmmPxFoR1Pk8ezOKr7BZW2fo',
+    authDomain: 'music-app-404e3.firebaseapp.com',
+    projectId: 'music-app-404e3',
+    storageBucket: 'music-app-404e3.appspot.com',
+    messagingSenderId: '306035918224',
+    appId: '1:306035918224:web:8d28407ba64bb7e8e55f15',
+  };
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const db = getFirestore();
+  const storage = getStorage(app);
+  connectFirestoreEmulator(db, 'localhost', 8080);
+  connectAuthEmulator(auth, 'http://localhost:9099');
+  connectStorageEmulator(storage, 'localhost', 9199);
+}
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
